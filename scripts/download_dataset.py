bc399aa2d7a3a1df5bc58a100a8e67e3e1f079c7
Sana Asghari <146634973+sanasgharii@users.noreply.github.com>
2026-03-04 12:27:40 +0100

    Fix flake8 line length

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -2,19 +2,31 @@ import kagglehub
 import shutil
 import os
 
+
 # Download dataset
 path = kagglehub.dataset_download(
-    "mehmettahiraslan/customer-shopping-dataset"
+    "mehmettahiraslan/customer-shopping-dataset",
 )
 
 print("Downloaded to:", path)
 
+
 # Move CSV into dbt seeds folder
-source_file = os.path.join(path, "customer_shopping_data.csv")
-destination_folder = "dbt/my_project/seeds"
+source_file = os.path.join(
+    path,
+    "customer_shopping_data.csv",
+)
+
+destination_folder = os.path.join(
+    "dbt",
+    "my_project",
+    "seeds",
+)
+
 destination_file = os.path.join(
-    destination_folder, "customer_shopping_data.csv"
-    )
+    destination_folder,
+    "customer_shopping_data.csv",
+)
 
 os.makedirs(destination_folder, exist_ok=True)
 shutil.copy(source_file, destination_file)
